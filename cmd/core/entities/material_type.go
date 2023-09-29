11090f1f3db744d5d550bb262f1e006c9146db04
Federico Veron <veronfederico45@gmail.com>
2023-09-29 12:10:15 -0300

    material type creation

--- cmd/core/entities/material_type.go
+++ cmd/core/entities/material_type.go
@@ -1,9 +1,13 @@
 package entities
 
-import "github.com/fedeveron01/golang-base/cmd/core"
+import (
+	"github.com/fedeveron01/golang-base/cmd/core"
+	"github.com/fedeveron01/golang-base/cmd/core/enums"
+)
 
 type MaterialType struct {
 	core.EntitiesBase
-	Name        string
-	Description string
+	Name              string
+	Description       string
+	UnitOfMeasurement enums.Enum
 }
