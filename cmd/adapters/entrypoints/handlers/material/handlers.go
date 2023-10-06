efad30bd4007fe613beb59bde4bd4179dc3d41ba
angelo <angelo.benedetti2910@gmail.com>
2023-10-06 14:26:39 -0300

    FEAT: agregue funcionalidad eliminar material

--- cmd/adapters/entrypoints/handlers/material/handlers.go
+++ cmd/adapters/entrypoints/handlers/material/handlers.go
@@ -3,17 +3,22 @@ package material_handler
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
 	"github.com/fedeveron01/golang-base/cmd/adapters/entrypoints"
 	"github.com/fedeveron01/golang-base/cmd/adapters/gateways"
 	"github.com/fedeveron01/golang-base/cmd/core/entities"
-	"github.com/fedeveron01/golang-base/cmd/usecases/material"
-	"io"
-	"net/http"
+	core_errors "github.com/fedeveron01/golang-base/cmd/core/errors"
+	material_usecase "github.com/fedeveron01/golang-base/cmd/usecases/material"
+	"github.com/gorilla/mux"
 )
 
 type MaterialHandlerInterface interface {
 	Create(w http.ResponseWriter, r *http.Request)
 	GetAll(w http.ResponseWriter, r *http.Request)
+	Delete(w http.ResponseWriter, r *http.Request)
 }
 type MaterialHandler struct {
 	entrypoints.HandlerBase
@@ -62,4 +67,28 @@ func (p *MaterialHandler) Create(w http.ResponseWriter, r *http.Request) {
 	}
 	p.WriteResponse(w, "material created", http.StatusCreated)
 
+}
+
+// Handle api/material/{id}
+func (p *MaterialHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	if !p.IsAuthorized(w, r) {
+		return
+	}
+	if !p.IsAdmin(w, r) {
+		return
+	}
+	vars := mux.Vars(r)
+	id := vars["id"]
+	_, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		p.WriteErrorResponse(w, core_errors.NewBadRequestError("id is not valid"))
+		return
+	}
+	err = p.materialUseCase.DeleteMaterial(id)
+	if err != nil {
+		p.WriteErrorResponse(w, err)
+		return
+	}
+	p.WriteResponse(w, "material deleted", http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 }
--- cmd/repositories/material.go
+++ cmd/repositories/material.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	gateway_entities "github.com/fedeveron01/golang-base/cmd/adapters/gateways/entities"
+	core_errors "github.com/fedeveron01/golang-base/cmd/core/errors"
 	"gorm.io/gorm"
 )
 
@@ -35,6 +36,12 @@ func (r *MaterialRepository) UpdateMaterial(material gateway_entities.Material) 
 }
 
 func (r *MaterialRepository) DeleteMaterial(id string) error {
-	r.db.Delete(&gateway_entities.Material{}, id)
+	result := r.db.Delete(&gateway_entities.Material{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return core_errors.NewInternalServerError("Material Id not found")
+	}
 	return nil
 }
