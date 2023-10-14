9f236a170269a5d2843af2b7bfa4ae2c2b51a607
Federico Veron <veronfederico45@gmail.com>
2023-10-13 22:50:25 -0300

    fixes

--- cmd/repositories/product.go
+++ cmd/repositories/product.go
@@ -16,48 +16,57 @@ func NewProductRepository(database *gorm.DB) *ProductRepository {
 	}
 }
 
-func (r *ProductRepository) CreateProduct(materialType gateway_entities.Product) (gateway_entities.Product, error) {
-	id := r.db.Create(&materialType)
+func (r *ProductRepository) CreateProduct(product gateway_entities.Product) (gateway_entities.Product, error) {
+	id := r.db.Create(&product)
 	if id.Error != nil {
 		return gateway_entities.Product{}, id.Error
 	}
-	return materialType, nil
+	return product, nil
 }
 
 func (r *ProductRepository) FindAll() ([]gateway_entities.Product, error) {
-	var materialTypes []gateway_entities.Product
-	r.db.Find(&materialTypes)
-	return materialTypes, nil
+	var products []gateway_entities.Product
+	r.db.Find(&products)
+	return products, nil
 }
 
 func (r *ProductRepository) FindByName(name string) *gateway_entities.Product {
-	var materialType gateway_entities.Product
-	r.db.Where("name = ?", name).First(&materialType)
-	if materialType.ID == 0 {
+	var product gateway_entities.Product
+	r.db.Where("name = ?", name).First(&product)
+	if product.ID == 0 {
 		return nil
 	}
-	return &materialType
+	return &product
 }
 
 func (r *ProductRepository) FindById(id uint) *gateway_entities.Product {
-	var materialType gateway_entities.Product
-	r.db.InnerJoins("MaterialProduct").InnerJoins("MaterialProduct.MaterialType").
-		Find(&materialType, id).First(&materialType)
-	if materialType.ID == 0 {
+	var product gateway_entities.Product
+	res := r.db.Find(&product, id).First(&product)
+	if res.Error != nil {
 		return nil
 	}
-	return &materialType
+	if product.ID == 0 {
+		return nil
+	}
+	var materialProducts []gateway_entities.MaterialProduct
+	res = r.db.InnerJoins("Material").Preload("Material.MaterialType").Find(&materialProducts, "product_id = ?", id)
+	if res.Error != nil {
+		return &product
+	}
+	product.MaterialProduct = materialProducts
+
+	return &product
 }
 
-func (r *ProductRepository) UpdateProduct(materialType gateway_entities.Product) (gateway_entities.Product, error) {
-	res := r.db.Save(&materialType)
+func (r *ProductRepository) UpdateProduct(product gateway_entities.Product) (gateway_entities.Product, error) {
+	res := r.db.Save(&product)
 	if res.Error != nil {
 		return gateway_entities.Product{}, res.Error
 	}
 	if res.RowsAffected == 0 {
-		return gateway_entities.Product{}, core_errors.NewInternalServerError("materialType update failed")
+		return gateway_entities.Product{}, core_errors.NewInternalServerError("product update failed")
 	}
-	return materialType, nil
+	return product, nil
 }
 
 func (r *ProductRepository) DeleteProduct(id uint) error {
